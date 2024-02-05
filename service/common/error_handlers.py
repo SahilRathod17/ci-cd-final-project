e7923be6b7b51ab07776712615106fdc1a75c456
Sahil Rathod <24sahilrathod@gmail.com>
2024-02-05 15:52:50 -0500

    flake8 issue resolved

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -31,11 +31,14 @@ def bad_request(error):
     message = str(error)
     app.logger.warning(message)
     return (
-        jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
-        ),
-        status.HTTP_400_BAD_REQUEST,
-    )
+    jsonify(
+        status=status.HTTP_400_BAD_REQUEST,
+        error="Bad Request",
+        message=message
+    ),
+    status.HTTP_400_BAD_REQUEST
+)
+
 
 
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
@@ -44,9 +47,14 @@ def not_found(error):
     message = str(error)
     app.logger.warning(message)
     return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
-    )
+    jsonify(
+        status=status.HTTP_400_BAD_REQUEST,
+        error="Bad Request",
+        message=message
+    ),
+    status.HTTP_400_BAD_REQUEST
+)
+
 
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
